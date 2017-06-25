5a7e25be0c58f54b5de15f9930388f2f602b0d7d
yangliu28 <yxl1450@case.edu>
2017-06-24 22:41:13 -0400

    add robot instantiation

--- line_formation.py
+++ line_formation.py
@@ -1,7 +1,9 @@
 # line formation simulation
 
 import pygame
+import random
 from line_formation_robot import LFRobot
+from line_formation_functions import *
 
 pygame.init()  # initialize pygame
 
@@ -18,12 +20,25 @@ screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Line Formation Simulation')
 
 # for physics, origin is at left bottom corner, starting (0, 0)
+# it's more natural to do the calculation in right hand coordiante system
 # the continuous, physical world for the robots
 world_size = (100.0, 100.0 * screen_size[1]/screen_size[0])
 
+# variables to configure the simulation
+robot_quantity = 30
+# coefficient to resize the robot distribution, but always keep close to center
+distrib_coef = 0.5
+const_vel = 2  # all moving robots are moving at a constant speed
+
+
 # instantiate the robot swarm
 robot = []  # container for all robots, index is also the IDs
-
+for i in range(robot_quantity):
+    # random position, away from the window edges
+    pos_temp = (((random.random()-0.5)*distrib_coef+0.5) * world_size[0],
+                ((random.random()-0.5)*distrib_coef+0.5) * world_size[1])
+    vel_temp = const_vel
+    ori_temp = random.random() * 2*math.pi  # random in (0, 2*pi)
 
 # the loop
 sim_exit = False  # simulation exit flag
--- line_formaton_functions.py
+++ line_formaton_functions.py
@@ -1,5 +1,21 @@
-
 # functions for the line formation simulation
 
-def world_to_display(input_pos, world_size, display_size):
+import math
+
+# reset radian angle to [0, 2*pi)
+def lf_reset_radian(radian):
+    while radian >= 2*math.pi:
+        radian = radian - 2*math.pi
+    while radian < 0:
+        radian = radian + 2*math.pi
+    return radian
+
+# convert positions in physics coordinates to display coordinates
+def lf_world_to_display(input_pos, world_size, display_size):
+    # lf stands for line formation
     pos_display = [0, 0]
+    pos_display[0] = int(input_pos[0]/world_size[0] * display_size[0])
+    pos_display[1] = int((1-input_pos[1]/world_size[1]) * display_size[1])
+    return pos_display
+
+
