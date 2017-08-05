668209cb9daa993d9afb77a16617693f4a00b059
yangliu28 <yxl1450@case.edu>
2017-08-05 17:48:56 -0400

    update title

--- line_formation_2.py
+++ line_formation_2.py
@@ -7,8 +7,7 @@
 # make sure the initial line segments start in the middle to avoid the line hit the wall.
 
 # comments for research purpose:
-# Same competing mechanism is used to ensure only one line being formed.
-
+# Same competing mechanism is used to ensure only one line is being formed.
 
 # comments for programming purpose:
 # Similar four statuses have been designed to serve different stages of the robot:
@@ -21,6 +20,28 @@
 # Only allowed status transitions are:
     # forward: '-1'->'0', '0'->'1', '1'->'2'
     # backward: '1'->'-1', '2'->'-1'
-# The way merging method works is, when '0' runs into a '2', it inquires 
+
+
+import pygame
+import math, random
+
+
+pygame.init()
+
+# for display, window origin is at left up corner
+screen_size = (1200, 1000)  # width and height
+background_color = (0, 0, 0)  # black background
+robot_0_color = (0, 255, 0)  # for robot status '0', green
+robot_1_color = (255, 153, 153)  # for robot status '1', pink
+robot_2_color = (255, 51, 0)  # for robot status '2', red
+robot_n1_color = (0, 51, 204)  # for robot status '-1', blue
+robot_size = 5  # robot modeled as dot, number of pixels in radius
+
+# set up the simulation window and surface object
+icon = pygame.image.load('icon_geometry_art.jpg')
+pygame.display.set_icon(icon)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Line Formation 1 Simulation')
+
 
 
