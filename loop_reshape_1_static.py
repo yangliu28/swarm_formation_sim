7b43d77f4f36e10af4911dfc439d72c4303a4462
yangliu28 <yxl1450@case.edu>
2017-08-21 15:55:32 -0400

    start n-poly

--- loop_reshape_1_static.py
+++ loop_reshape_1_static.py
@@ -2,11 +2,24 @@
 
 
 
-# need two polygons, one for initial setup, one for target
-# for test the algorithm, the more variaties the better
+# need two polygons, one for initial setup formation, one for target formation
+# have the algorithm for this first, no matter in which graphic window
+
+# the basic requirements:
+# equal space polygon, concave is allowed
+# no collapse inside, any pair of non-neighbor robots should not be closer than loop space
+
 
 # mouse click to select? or just show all of them (preferred)
 
 # plot histogram with matplotlib
+
+
+import pygame
+
+pygame.init()
+
+
+
 
 
