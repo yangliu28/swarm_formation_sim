127d73ad031e023a2b3234fc195b2671d1c0b00d
yangliu28 <yxl1450@case.edu>
2017-08-27 19:02:44 -0400

    working bar chart animation

--- bar_chart.py
+++ bar_chart.py
@@ -3,15 +3,25 @@
 import matplotlib.pyplot as plt
 import numpy, random, time
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
-# x_pos = numpy.arrange(5)
-x_pos = range(1,6)
+
+x_pos = numpy.arange(5)
 
 
 while True:
+    # plt.clf()
+    # y_pos = [random.random() for i in range(5)]
+    # plt.bar(x_pos, y_pos, align='center')
+    # plt.show()
     y_pos = [random.random() for i in range(5)]
-    plt.bar(x_pos, y_pos, align='center')
-    plt.show()
+    ax.clear()
+    ax.bar(x_pos, y_pos, align='center')
+    fig.canvas.draw()
+    fig.show()
     time.sleep(0.5)
+
+
 
 
