6c2dba493e6d845c7da60b9e4f67b832e8a2bb30
yangliu28 <yxl1450@case.edu>
2017-09-16 13:43:44 -0400

    add motion test program

---
+++ loop_reshape_test_motion.py
@@ -0,0 +1,10 @@
+# program for testing only the physical motion control algorithm of the loop reshape process
+# the starting and ending formations will be read from file
+
+# two arguments specifying the loop formation files needs to be passed
+# first one is for initial loop formation, second for target formation
+# the loop formation files should be located under 'loop-data' folder
+
+import pygame
+
+
