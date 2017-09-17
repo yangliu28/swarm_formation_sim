5a70355ab5fd231aa5cd0464250372ce4e4c1612
yangliu28 <yxl1450@case.edu>
2017-09-17 17:45:34 -0400

    finish framework

--- loop_reshape_test_motion.py
+++ loop_reshape_test_motion.py
@@ -36,13 +36,29 @@ try:
     targ_shift = sys.argv[3]
 except: pass
 
+pygame.init()  # initialize the pygame
+# parameters for display, window origin is at left up corner
+screen_size = (600, 800)  # width and height in pixels
+    # top half for initial formation, bottom half for target formation
+background_color = (0,0,0)  # black background
+robot_color = (255,0,0)  # red for robot and the line segments
+robot_size = 5  # robot modeled as dot, number of pixels for radius
+world_size = (100.0, 100.0 * screen_size[1]/screen_size[0])
+
+# set up the simulation window and surface object
+icon = pygame.image.load("icon_geometry_art.jpg")
+pygame.display.set_icon(icon)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Loop Reshape Motion Test")
+
 # variables to configure the simulation
 poly_n = 0  # will be decided when reading formation files
 loop_space = 4.0  # side length of the equilateral polygon
 # linear spring constant modeling the pulling and pushing effect of neighbor nodes
 linear_const = 1.0
 # angular spring constant modeling the bending effect of neighbor nodes
-angular_const = 0.1
+angular_const = 0.2
+disp_coef = 0.5  # coefficient from feedback vector to displacement
 
 # construct the polygons from formation data from file
 nodes = [[], []]  # node positions for the two formation, index is the robot's ID
@@ -63,7 +79,7 @@ for i in range(2):
     # read all interior angles form the file
     inter_ang = []
     new_line = f.readline()
-    when len(new_line) != 0:  # not the end of the file yet
+    while len(new_line) != 0:  # not the end of the file yet
         inter_ang.append(float(new_line))
         new_line = f.readline()
     # check if this file has the nubmer of interior angles as it promised
@@ -103,11 +119,8 @@ for i in range(2):
 # calculate the interior angles of the target formation
 inter_targ = [0 for i in range(poly_n)]
 for i in range(poly_n):
-    node_h = nodes[1][i]  # host node
-    node_l = nodes[1][(i-1)%poly_n]  # node on the left
-    node_r = nodes[1][(i+1)%poly_n]  # node on the right
-    vect_l = node_l-node_h  # from host to left
-    vect_r = node_r-node_h  # from host to right
+    vect_l = nodes[1][(i-1)%poly_n] - nodes[1][i]  # from host to left
+    vect_r = nodes[1][(i+1)%poly_n] - nodes[1][i]  # from host to right
     # get the small angle between vect_l and vect_r
     inter_targ[i] = math.acos((vect_l[0]*vect_r[0] + vect_l[1]*vect_r[1])/
                               (loop_space*loop_space))
@@ -119,7 +132,7 @@ for i in range(poly_n):
 # loop for the physical motion update
 sim_exit = False
 sim_pause = False
-frame_period = 500  # updating period of the simulation and graphics, in millisecond
+frame_period = 100  # updating period of the simulation and graphics, in millisecond
 timer_last = pygame.time.get_ticks()
 timer_now = timer_last
 # the interior angle of initial formation will be updated dynamically
@@ -143,22 +156,24 @@ while not sim_exit:
     if (timer_now - timer_last) > frame_period:
         timer_last = timer_now  # reset timer
 
-        # update the dynamic interior angles and neighbor distances of the loop
+        # update the dynamic neighbor distances of the loop
         for i in range(poly_n):
-            node_h = nodes[0][i]  # host node
-            node_l = nodes[0][(i-1)%poly_n]  # node on the left
-            node_r = nodes[0][(i+1)%poly_n]  # node on the right
-            vect_l = node_l-node_h  # from host to left
-            vect_r = node_r-node_h  # from host to right
+            vect_r = nodes[0][(i+1)%poly_n] - nodes[0][i]  # from host to right
+            dist_neigh[i] = math.sqrt(vect_r[0]*vect_r[0] + vect_r[1]*vect_r[1])
+
+        # update the dynamic interior angles of the loop
+        for i in range(poly_n):
+            i_l = (i-1)%poly_n  # index of left neighbor
+            i_r = (i+1)%poly_n  # index of right neighbor
+            vect_l = nodes[0][i_l] - nodes[0][i]  # from host to left
+            vect_r = nodes[0][i_r] - nodes[0][i]  # from host to right
             # get the small angle between vect_l and vect_r
             inter_curr[i] = math.acos((vect_l[0]*vect_r[0] + vect_l[1]*vect_r[1])/
-                                      (loop_space*loop_space))
+                                      (dist_neigh[i_l] * dist_neigh[i]))
             if (vect_r[0]*vect_l[1] - vect_r[1]*vect_l[0]) < 0:
                 # cross product of vect_r to vect_l is smaller than 0
                 # the resulting interior angle should be in range of [0, 2*pi)
                 inter_curr[i] = 2*math.pi - inter_curr[i]
-            # get neighbor distances from node_h to node_r
-            dist_neigh[i] = math.sqrt(vect_r[0]*vect_r[0] + vect_r[1]*vect_r[1])
 
         # variable for feedback from all spring effects
         fb_vect = [np.zeros([1,2]) for i in range(poly_n)]
@@ -167,14 +182,44 @@ while not sim_exit:
             i_l = (i-1)%poly_n  # index of left neighbor
             i_r = (i+1)%poly_n  # index of right neighbor
             # unit vector from host to left
-            vect_l_u = (nodes[0][i_l]-nodes[0][i])/dist_neigh[i_l]
+            vect_l = (nodes[0][i_l]-nodes[0][i])/dist_neigh[i_l]
+            # unit vector bending host node toward inside the polygon from left neighbor
+            vect_lb = np.array([vect_l[1], -vect_l[0]])  # rotate vect_l cw for pi/2
             # unit vector from host to right
-            vect_r_u = (nodes[0][i_r]-nodes[0][i])/dist_neigh[i]
+            vect_r = (nodes[0][i_r]-nodes[0][i])/dist_neigh[i]
+            # unit vector bending host node toward inside the polygon from right neighbor
+            vect_rb = np.array([-vect_r[1], vect_r[0]])  # rotate vect_r ccw for pi/2
+            
             # add the pulling or pushing effect from left neighbor
-            fb_vect[i] = fb_vect[i] + linear_const * (dist_neigh[i_l]-loop_space) * vect_l_u
+            fb_vect[i] = fb_vect[i] + (dist_neigh[i_l]-loop_space) * linear_const * vect_l
             # add the pulling or pushing effect from right neighbor
-            fb_vect[i] = fb_vect[i] + linear_const * (dist_neigh[i]-loop_space) * vect_r_u
-            inter_curr[i]-inter_targ[(i+targ_shift)%poly_n]
+            fb_vect[i] = fb_vect[i] + (dist_neigh[i]-loop_space) * linear_const * vect_r
+            # add the bending effect from left neighbor
+            fb_vect[i] = fb_vect[i] + ((inter_curr[i_l] - inter_targ[(i_l+targ_shift)%poly_n])*
+                                       angular_const * vect_lb)
+            # add the bending effect from right neighbor
+            fb_vect[i] = fb_vect[i] + ((inter_curr[i_r] - inter_targ[(i_r+targ_shift)%poly_n])*
+                                       angular_const * vect_rb)
+
+            # update one step of position
+            nodes[0][i] = nodes[0][i] + disp_coef * fb_vect[i]
+
+        # graphics update
+        screen.fill(background_color)
+        for i in range(2):
+            # draw the nodes and line segments
+            disp_pos = [[0,0] for j in range(poly_n)]
+            # calculate the display pos for all nodes, as red dots
+            for j in range(0, poly_n):
+                disp_pos[j] = world_to_display(nodes[i][j], world_size, screen_size)
+                pygame.draw.circle(screen, robot_color, disp_pos[j], robot_size, 0)
+            # draw an outer circle to mark the starting node
+            pygame.draw.circle(screen, robot_color, disp_pos[0], int(robot_size*1.5), 1)
+            for j in range(poly_n-1):
+                pygame.draw.line(screen, robot_color, disp_pos[j], disp_pos[j+1])
+            pygame.draw.line(screen, robot_color, disp_pos[poly_n-1], disp_pos[0])
+        pygame.display.update()
+
 
 
 
