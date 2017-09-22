7fd9ee5ce17380bf9a6c2df413a498a94602ac22
yangliu28 <yxl1450@case.edu>
2017-09-22 01:10:56 -0400

    finish loop filter

--- curve_shape_test_filter.py
+++ curve_shape_test_filter.py
@@ -10,7 +10,7 @@ import numpy as np
 from formation_functions import *
 
 pygame.init()  # initialize pygame
-screen_size = (600, 800)
+screen_size = (600, 900)
 background_color = (0,0,0)  # black background
 node_color = (255,0,0)  # red for nodes
 node_size = 5  # radius of dot in pixels in display coordinates
@@ -25,11 +25,13 @@ pygame.display.set_caption("Curve Shape Filter Test")
 # x pointing right, y pointing up
 world_size = (100.0, 100.0 * screen_size[1]/screen_size[0])
 
-# general variables specifying the simulation
-N = 30  # number of nodes on the open curve or loop
+# general variables specifying this simulation
+N = 30  # number of nodes on the open curve or loop, at least 4 for the loop
 node_space = 4.0  # node space in world coordinates
-dev_lower = math.pi/3  # lower limit of deviation angle
-dev_upper = 2*math.pi - dev_lower  # upper limit, symmetric about pi
+curve_dev_half = math.pi/2  # half range of randomly generated deviation angle for curve
+# curve's middle point of deviation angle is 0
+loop_dev_mid = 2*math.pi/N  # loop's middle point of deviation angle
+loop_dev_std = math.pi/8  # standard deviation of deviation angle for closed loop
 
 # initialize the node positions variables
 nodes = np.array([[0,0] for i in range(N)])  # originally generated node positions
@@ -39,7 +41,7 @@ nodes[1] = [node_space, 0]
 nodes_f[1] = [node_space, 0]
 
 # change here to switch between open curve testing and closed loop testing
-simulation_mode = 0  # 0 for open curve, 1 for close loop
+simulation_mode = 1  # 0 for open curve, 1 for close loop
 
 if simulation_mode == 0:
 	# section for the filter test of the open curve
@@ -49,15 +51,16 @@ if simulation_mode == 0:
     for i in range(2, N):
         dev_satisfied = False  # flag indicating if a generated deviation is good
         while not dev_satisfied:
-            dev_ang[i-2] = random.uniform(dev_lower, dev_upper)  # deviation angle on node i-1
+            # deviation angle of node i-1
+            dev_ang[i-2] = random.uniform(-curve_dev_half, curve_dev_half)
             forward_ang_t = reset_radian(forward_ang + dev_ang[i-2])  # temporary
             # calculate position of node i
             nodes[i] = nodes[i-1] + node_space*np.array([math.cos(forward_ang_t),
                                                          math.sin(forward_ang_t)])
-            # check if it is too close to all previous neighbors, except the one right before
+            # check if it is too close to previous nodes, except the one right before
             dev_satisfied = True  # will change to false if close nodes are detected
             for j in range(i-1):
-                if np.linalg.norm(nodes[j]-nodes[i]) < node_space:
+                if np.linalg.norm(nodes[i]-nodes[j]) < node_space:
                     dev_satisfied = False
                     break
             # set the forward angle to this decided one
@@ -70,7 +73,7 @@ if simulation_mode == 0:
         if i == 0: dev_ang_f[0] = (dev_ang[0]+dev_ang[1])/2
         elif i == N-3: dev_ang_f[N-3] = (dev_ang[N-4]+dev_ang[N-3])/2
         else: dev_ang_f[i] = (dev_ang[i-1]+dev_ang[i]+dev_ang[i+1])/3
-    # construct the new curve with filtered deviation angle
+    # construct the new curve with filtered deviation angles
     # it's possible but not likely that new curve has close non-neighbors nodes
     forward_ang = 0.0
     for i in range(2, N):
@@ -80,19 +83,19 @@ if simulation_mode == 0:
 
     # shift the two curves to its geometric center, rotate a random angle
     # then shift the two curves again to top and bottom halves of the window
-    rot_ang = random.uniform(-math.pi, math.pi)  # two curves rotate same angle
-    rot = np.array([[math.cos(rot_ang), math.sin(rot_ang)],
-                    [-math.sin(rot_ang), math.cos(rot_ang)]])  # rotation matrix
+    rot_ang = random.uniform(-math.pi, math.pi)  # two curves rotate the same angle
+    rot_mat = np.array([[math.cos(rot_ang), math.sin(rot_ang)],
+                        [-math.sin(rot_ang), math.cos(rot_ang)]])  # rotation matrix
     # shift old curve to origin
     geometric_center = np.mean(nodes, axis=0)
     nodes = nodes - geometric_center  # shift to its geometric center
-    nodes = np.dot(nodes, rot)  # rotate curve in angle of rot_ang
+    nodes = np.dot(nodes, rot_mat)  # rotate curve in angle of rot_ang
     # shift old curve to top halve of the window
     nodes = nodes + np.array([world_size[0]/2, 3*world_size[1]/4])
     # do all the above for the new filtered curve
-    geometric_center = np.mean(nodes, axis=0)
+    geometric_center = np.mean(nodes_f, axis=0)
     nodes_f = nodes_f - geometric_center  # shift to its geometric center
-    nodes_f = np.dot(nodes_f, rot)  # rotate curve in angle of rot_ang
+    nodes_f = np.dot(nodes_f, rot_mat)  # rotate curve in angle of rot_ang
     nodes_f = nodes_f + np.array([world_size[0]/2, world_size[1]/4])  # to bottom half
 
     # visualize the result of the filter
@@ -111,10 +114,111 @@ if simulation_mode == 0:
     pygame.draw.circle(screen, node_color, disp_pos[0], int(node_size*1.5), 1)
     for i in range(N-1):
         pygame.draw.line(screen, node_color, disp_pos[i], disp_pos[i+1])
+    pygame.display.update()
 
 else:
 	# section for the filter test of close loops
-    pass
+    # A different way of generating deviation angle is used here. Since random generation
+    # has no guarantee the mean value will be close to the middle of the range, a normal
+    # distribution will do a better work here.
+    dev_ang = []
+    while True:  # keep on generating deviation angles until success
+        shape_good = True  # indicating if loop is still in good shape
+        dev_ang = np.random.normal(loop_dev_mid, loop_dev_std, N-3).tolist()
+        forward_ang = 0.0  # starting forward angle
+        for i in range(2, N-1):
+            forward_ang = reset_radian(forward_ang + dev_ang[i-2])
+            nodes[i] = nodes[i-1] + node_space*np.array([math.cos(forward_ang),
+                                                         math.sin(forward_ang)])
+            # check if new node is too close to previous nodes, except the one right before
+            for j in range(i-1):
+                if np.linalg.norm(nodes[i]-nodes[j]) < node_space:
+                    shape_good = False
+                    break
+            if not shape_good: break
+        if not shape_good: continue
+        # check if second last node is too far away from first node
+        vect_temp = nodes[0]-nodes[N-2]  # from last second node to first node
+        dist_temp = np.linalg.norm(vect_temp)
+        if dist_temp > 2*node_space: continue
+        # if here, the guess of deviation angles has been approved
+        # calculate position of the last node
+        forward_ang = math.atan2(vect_temp[1], vect_temp[0])
+        rot_ang = math.acos(dist_temp/2/node_space)
+        forward_ang = reset_radian(forward_ang - rot_ang)  # rotate cw of rot_ang
+        nodes[N-1] = nodes[N-2] + node_space*np.array([math.cos(forward_ang),
+                                                       math.sin(forward_ang)])
+        # adding all the missing deviation angles
+        # for deviation angle at node 0
+        vect_b = nodes[0] - nodes[N-1]  # back vector
+        vect_f = nodes[1] - nodes[0]  # front vector
+        new_dev = reset_radian(math.atan2(vect_f[1], vect_f[0]) -
+                               math.atan2(vect_b[1], vect_b[0]))
+        dev_ang.insert(0, new_dev)
+        # for deviation angle at node N-2
+        vect_b = nodes[N-2] - nodes[N-3]
+        vect_f = nodes[N-1] - nodes[N-2]
+        dev_ang.append(reset_radian(math.atan2(vect_f[1], vect_f[0]) -
+                                    math.atan2(vect_b[1], vect_b[0])))
+        # for deviation angle at node N-1
+        vect_b = nodes[N-1] - nodes[N-2]
+        vect_f = nodes[0] - nodes[N-1]
+        dev_ang.append(reset_radian(math.atan2(vect_f[1], vect_f[0]) -
+                                    math.atan2(vect_b[1], vect_b[0])))
+
+    # the moving averaging filter
+    dev_ang_f = np.array([0 for i in range(N)])
+    for i in range(N):
+        i_l = (i-1)%N  # neighbor on left
+        i_r = (i+1)%N  # neighbor on right
+        dev_ang_f[i] = (dev_ang[i_l] + dev_ang[i] + dev_ang[i_r])/3
+    # make sure the summation of deviation angles is 2*pi
+    dev_sum = np.sum(dev_ang_f)
+    dev_ang_f = dev_ang_f + (2*math.pi-dev_sum)/N
+    # construct the new loop with filtered deviation angles
+    forward_ang = 0.0
+    for i in range(2, N):
+        forward_ang = reset_radian(forward_ang + dev_ang_f[i-1])
+        nodes_f[i] = nodes_f[i-1] + node_space*np.array([math.cos(forward_ang),
+                                                         math.sin(forward_ang)])
+
+    # shift the two loops to its geometric center, rotate a random angle
+    # then shift the two loops agian to top and bottom halves of the window
+    rot_ang = random.uniform(-math.pi, math.pi)  # two loops rotate the same angle
+    rot_mat = np.array([[math.cos(rot_ang), math.sin(rot_ang)],
+                        [-math.sin(rot_ang), math.cos(rot_ang)]])  # rotation matrix
+    # shift old loop to origin
+    geometric_center = np.mean(nodes, axis=0)
+    nodes = nodes - geometric_center  # shift to its geometric center
+    nodes = np.dot(nodes, rot_mat)  # rotate curve in angle of rot_ang
+    # shift old loop to top halve of the window
+    nodes = nodes + np.array([world_size[0]/2, 3*world_size[1]/4])
+    # do all the above for the new filtered curve
+    geometric_center = np.mean(nodes_f, axis=0)
+    nodes_f = nodes_f - geometric_center  # shift to its geometric center
+    nodes_f = np.dot(nodes_f, rot_mat)  # rotate loop in angle of rot_ang
+    nodes_f = nodes_f + np.array([world_size[0]/2, world_size[1]/4])  # to bottom half
+
+    # visualize the result of the filter
+    screen.fill(background_color)
+    disp_pos = [[0,0] for i in range(N)]
+    for i in range(N):
+        disp_pos[i] = world_to_display(nodes[i], world_size, screen_size)
+        pygame.draw.circle(screen, node_color, disp_pos[i], node_size, 0)
+    pygame.draw.circle(screen, node_color, disp_pos[0], int(node_size*1.5), 1)
+    for i in range(N-1):
+        pygame.draw.line(screen, node_color, disp_pos[i], disp_pos[i+1])
+    pygame.draw.line(screen, node_color, disp_pos[N-1], disp_pos[0])
+    # repeat above for the new filtered curve
+    for i in range(N):
+        disp_pos[i] = world_to_display(nodes_f[i], world_size, screen_size)
+        pygame.draw.circle(screen, node_color, disp_pos[i], node_size, 0)
+    pygame.draw.circle(screen, node_color, disp_pos[0], int(node_size*1.5), 1)
+    for i in range(N-1):
+        pygame.draw.line(screen, node_color, disp_pos[i], disp_pos[i+1])
+    pygame.draw.line(screen, node_color, disp_pos[N-1], disp_pos[0])
+    pygame.display.update()
+
 
 # simulation exit control
 sim_exit = False  # simulation exit flag
