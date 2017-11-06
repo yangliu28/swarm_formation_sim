ca3b103db7202b6969fbd106d98c9f7bb97e7b8d
yangliu28 <yxl1450@case.edu>
2017-11-06 14:31:16 -0500

    debug cmd options

--- probabilistic_convergence.py
+++ probabilistic_convergence.py
@@ -8,13 +8,16 @@ import matplotlib.pyplot as plt
 from network_generator_2D_swarm import *
 
 net_size = 0  # size of the honeycomb network
-net_filename = ''  # filename of the network file
-net_filepath = ''  # filepath of the network file
 net_folder = 'honeycomb-networks'  # folder for all network files
+net_filename = '30-1'  # defautl filename of the network file, if no input
+net_filepath = os.path.join(os.getcwd(), net_folder, net_filename)  # corresponding filepath
 
 # read command line options
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'f:')
+    # The colon after 'f' means '-f' requires an argument, it will raise an error if no
+    # argument followed by '-f'. But if '-f' is not even in the arguments, this won't raise
+    # an error. So it's necessary to define the default network filename
 except getopt.GetoptError as err:
     print str(err)
     sys.exit()
@@ -34,9 +37,10 @@ nodes = []
 f = open(net_filepath, 'r')
 new_line = f.readline()
 while len(new_line) != 0:  # not the end of the file yet
-    pos_str = new_line[0:-1].split(' ')  # get rid of '\n'
+    pos_str = new_line[0:-1].split(' ')  # get rid of '\n' at end
     pos = [int(pos_str[0]), int(pos_str[1])]  # convert to integers
-    print pos
     nodes.append(pos)
     new_line = f.readline()
+
+
 
