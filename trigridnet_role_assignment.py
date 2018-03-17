fc7de497c8d529c95a6affc50e4464259b472981
yangliu28 <yxl1450@case.edu>
2018-03-17 10:34:09 -0400

    rs read net

--- trigridnet_role_assignment.py
+++ trigridnet_role_assignment.py
@@ -1,16 +1,55 @@
-# This simulation tests a distributed role assignment algorithm, for robots starting at
-# arbitrary triangle grid network, and performing one-to-one role assignment.
+# This simulation tests a distributed role assignment algorithm, for robot swarm starting
+# at arbitrary triangle grid network, to perform one-to-one role assignment. The assigned
+# roles are abstract and represented as index numbers. The primary goal of this role
+# assignment algorithm is to have all robots agree on the one-to-one assignment scheme,
+# instead of coming up with the best assignment scheme.
 
-# Inter-robot communication is used to convey message between unconnected robots. Although
-# it is possible for each robot getting to know the information of all other robots,
+# Inter-robot communication is used to let one robot know the status of another robot that
+# is not directly connected. Enabling message convey is what I consider the most convenient
+# way to resolve conflict when two robots decide on the same assigned role. Although in this
+# way, it is possible for each robot to have the information of all the other robots, and
+# thus it could become a distributed master control algorithm, we still consider the proposed
+# algorithm a distributed one. Because we limit the information each robot will store, and
+# the local computation performed, so the algorithm is still robust.
 
-# we still consider it a distributed algorithm, because we limited the local computation
-# and information it will store, so the algorithm can be more robust.
+# The communication delay is also reflected in the simulation, one step in simulation will
+# update one step of message transmission. Color is used to highlight the conflicts in the
+# role assignment, instead of showing convergence of decisions.
 
 
+import pygame
+import matplotlib.pyplot as plt
+from trigridnet_generator import *
+import numpy as np
+import os, getopt, sys, time
 
-# the step of communication is also considered in the simulation
+net_folder = 'trigrid-networks'
+net_filename = '30-1'  # default network
+net_size = 30  # default network size
+net_filepath = os.path.join(os.getcwd(), net_folder, net_filename)
 
-# color is used to hightlight the conflicts, instead of showing convergence of decisions
+nobargraph = False
+
+# read command line options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'f:', ['nobargraph'])
+except getopt.GetoptError as err:
+    print str(err)
+    sys.exit()
+for opt,arg in opts:
+    if opt = '-f':
+        net_filename = arg
+        net_filepath = os.path.join(os.getcwd(), net_folder, net_filename)
+        # check if this file exists
+        if not os.path.isfile(net_filepath):
+            print "{} does not exist".format(net_filename)
+            sys.exit()
+        # parse the network size
+        net_size = int(net_filename.split('-')[0])
+    elif opt == '--nobargraph':
+        nobargraph = True
+
+# read the network from file
+nodes = []  # the node positions in the form of triangle grid network
 
 
