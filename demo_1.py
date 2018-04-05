83711f16f75b89383399adcd4620079e74c25b50
yangliu28 <yxl1450@case.edu>
2018-04-05 16:03:38 -0400

    sim window size tuned

--- demo_1.py
+++ demo_1.py
@@ -35,8 +35,8 @@
 # loop formation: no colors, empty circle for dormant, filled circle for active
 
 
-import pygame
 from __future__ import print_function
+import pygame
 import sys, getopt
 import numpy as np
 
@@ -46,11 +46,11 @@ swarm_size = 30  # default number of robots in the swarm
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'n:')
 except getopt.GetoptError as err:
-    print str(err)
+    print(str(err))
     sys.exit()
 for opt,arg in opts:
-    if opt = '-n':
-        swarm_size = arg
+    if opt == '-n':
+        swarm_size = int(arg)
 
 # conversion between physical and display world sizes
 # To best display any robot swarm in its appropriate window size, and have enough physical
@@ -63,22 +63,59 @@ for opt,arg in opts:
 # the size of the swarm grow large, it won't be able to be fitted in if performing a line or
 # circle formation. A compromise is to make swarm size proportional to the side length to the
 # power exponent between 1 and 2.
-
-# automatically calculate world_side_coef
-# from a good pair of swarm_size and its world_side_length
-
-power_exponent = 1.5
-world_side_coef = 0.5
+power_exponent = 1.9  # between 1.0 and 2.0
+    # the larger the parameter, the slower the windows grows with swarm size; vice versa
+# for converting from physical world to display world
+pixels_per_length = 50  # this is to be fixed
+# calculate world_side_coef from a desired screen size for 30 robots
+def cal_world_side_coef():
+    desired_screen_size = 400  # desired screen size for 30 robots
+    desired_world_size = float(desired_screen_size) / pixels_per_length
+    return desired_world_size / pow(30, 1/power_exponent)
+world_side_coef = cal_world_side_coef()
 world_side_length = world_side_coef * pow(swarm_size, 1/power_exponent)
-world_size = (world_side_length, world_side_length)  # physical world
+world_size = (world_side_length, world_side_length)  # square physical world
 
-pixels_per_length = 50  # for converting from physical world to display world
+# screen size calculated from world size
+screen_side_length = int(pixels_per_length * world_side_length)
+screen_size = (screen_side_length, screen_side_length)  # square display world
 
-screen_side_length = pixels_per_length * world_side_length
-screen_size = (screen_side_length, screen_side_length)  # display world
+print("world_side_length: {}".format(world_side_length))
+print("screen_side_length: {}".format(screen_side_length))
 
+# robot positions
 robot_poses = np.random.rand(swarm_size, 2) * world_side_length
-disp_poses = 
+# calculate and return the display positions from global variables
+def cal_disp_poses():
+    poses_temp = robot_poses / world_side_length
+    poses_temp[:,1] = 1.0 - poses_temp[:,1]
+    poses_temp = poses_temp * screen_side_length
+    return poses_temp.astype(int)
+disp_poses = cal_disp_poses()
+
+# visualization specifications
+color_white = (255,255,255)
+color_black = (0,0,0)
+distinct_color_set = ((230,25,75), (60,180,75), (255,225,25), (0,130,200), (245,130,48),
+    (145,30,180), (70,240,240), (240,50,230), (210,245,60), (250,190,190),
+    (0,128,128), (230,190,255), (170,110,40), (255,250,200), (128,0,0),
+    (170,255,195), (128,128,0), (255,215,180), (0,0,128), (128,128,128))
+node_size = 7
+line_width = 2
+
+# set up the simulation window
+pygame.init()
+icon = pygame.image.load("icon_geometry_art.jpg")
+pygame.display.set_icon(icon)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Demo 1")
+# draw the network
+screen.fill(color_white)
+for i in range(swarm_size):
+    pygame.draw.circle(screen, color_black, disp_poses[i], node_size, 2)
+pygame.display.update()
+
+raw_input("<Press Enter to continue>")
 
 
 
